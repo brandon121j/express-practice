06512bde591e90e141315d93e67bfe78c91932dc
Brandon <brandon121j@gmail.com>
2021-09-28 17:41:27 -0400

    Completion of person schema

---
+++ routes/person/model/Person.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "First Name is required"]
+        },
+        lastName: {
+            type: String,
+            required: [true, "Last Name is required"]
+        },
+        age: {
+            type: Number,
+            required: [true, "Age is required"]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('person', personSchema);
