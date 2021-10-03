c7f542b85aecbb4378995140f0bf244fea3a6c72
Brandon <brandon121j@gmail.com>
2021-10-02 23:22:45 -0400

    Additon of delete all request in router file

--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAllRecipes, createRecipe, deleteRecipe } = require('./controller/recipeController');
+const { getAllRecipes, createRecipe, deleteRecipe, deleteAll } = require('./controller/recipeController');
 
 router.get('/', getAllRecipes);
 router.post('/create-recipe', createRecipe);
 router.delete('/delete-recipe-by-id/:id', deleteRecipe);
+router.delete('/delete-all', deleteAll);
