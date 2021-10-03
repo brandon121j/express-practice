ea6b00bab849d56d4b50a3a5ff27bc55aef25805
Brandon <brandon121j@gmail.com>
2021-10-02 23:21:37 -0400

    Addition of delete-recipe-by-id request in router file

--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAllRecipes, createRecipe } = require('./controller/recipeController');
+const { getAllRecipes, createRecipe, deleteRecipe } = require('./controller/recipeController');
 
 router.get('/', getAllRecipes);
 router.post('/create-recipe', createRecipe);
+router.delete('/delete-recipe-by-id/:id', deleteRecipe);
