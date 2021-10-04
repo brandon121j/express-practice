2f12690743c693215695bceb9ab56f1bbb681f13
Brandon Johnson <brandon121j@gmail.com>
2021-10-03 23:09:34 -0400

    Addition of update route

--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAllRecipes, createRecipe, deleteRecipe, deleteAll } = require('./controller/recipeController');
+const { getAllRecipes, createRecipe, deleteRecipe, deleteAll, updateRecipe } = require('./controller/recipeController');
 
 router.get('/', getAllRecipes);
 router.post('/create-recipe', createRecipe);
 router.delete('/delete-recipe-by-id/:id', deleteRecipe);
 router.delete('/delete-all', deleteAll);
+router.put('/update/:id', updateRecipe);
